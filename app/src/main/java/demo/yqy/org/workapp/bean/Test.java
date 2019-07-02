a26c2fe57d7cca3c4cb6af49197627ce9887e322
yqy <tpyangqingyuan@163.com>
2019-07-03 00:19:24 +0800

    完善

--- app/src/main/java/demo/yqy/org/workapp/bean/Test.java
+++ app/src/main/java/demo/yqy/org/workapp/bean/Test.java
@@ -1,14 +1,3 @@
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
-        "http://www.w3.org/TR/html4/loose.dtd">
-<html>
-<head>
-    <title>FungoLive About</title>
-    <meta name="Generator" content="EditPlus">
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-</head>
-<body>
-<pre class="brush: java;">
-
 package demo.yqy.org.workapp.bean;
 
 public class Test {
@@ -29,9 +18,3 @@ public class Test {
         return test;
     }
 }
-
-
-</pre>
-
-</body>
-</html>
