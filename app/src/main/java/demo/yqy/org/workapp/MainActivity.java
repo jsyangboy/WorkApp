45b75dccdeca5d1d9c118a39a3a479a12da357ae
yqy <tpyangqingyuan@163.com>
2019-07-13 19:05:47 +0800

    线程同步打印12

--- app/src/main/java/demo/yqy/org/workapp/MainActivity.java
+++ app/src/main/java/demo/yqy/org/workapp/MainActivity.java
@@ -10,6 +10,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -103,6 +104,14 @@ public class MainActivity extends AppCompatActivity {
                     viewHolder.content.setText(content);
                 }
 
+                viewHolder.content.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent intent = new Intent(mContext, ContentActivity.class);
+                        intent.putExtra("bean", itemBean);
+                        startActivity(intent);
+                    }
+                });
                 viewHolder.itemView.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
