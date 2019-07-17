aa63e975577371c9f5230b2b1a75a3b5d1e75b2e
yqy <tpyangqingyuan@163.com>
2019-07-18 00:00:29 +0800

    mvp

--- app/src/main/java/demo/yqy/org/workapp/bean/Test.java
+++ app/src/main/java/demo/yqy/org/workapp/bean/Test.java
@@ -1,3 +1,15 @@
 package demo.yqy.org.workapp.bean;
 
+public class Test{
+    public static void main(String arg[]){
+        System.out.print(jumpFloor(10));
+    }
+
+    private static int jumpFloor(int number) {
+        if(number<=1){
+            return 1;
+        }
+        return jumpFloor(number-1) + jumpFloor(number -2);
+    }
+}
 
